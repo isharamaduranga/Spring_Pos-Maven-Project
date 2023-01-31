a9ca029b2f4beef5160f49fd2f544f45867309ec
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-01-31 16:08:57 +0530

    C_Updated..

--- Pos-Back_End/src/main/java/lk/ijse/spring/config/WebRootConfig.java
+++ Pos-Back_End/src/main/java/lk/ijse/spring/config/WebRootConfig.java
@@ -7,12 +7,11 @@
  */
 
 package lk.ijse.spring.config;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-
+@ComponentScan(basePackages = "lk.ijse.spring.service")
 public class WebRootConfig {
-    public WebRootConfig() {
-        System.out.println("root config");
-    }
+
 }
