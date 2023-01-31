56c24e700e8b7153bfb06e72e77d5a09345cf6b6
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-01-31 13:55:24 +0530

    Applied pkg structure pos-Back_End..

---
+++ Pos-Back_End/src/main/java/lk/ijse/spring/WebAppInitializer.java
@@ -0,0 +1,12 @@
+/**
+ * @author : Ishara Maduarnga
+ * Project Name: Maven_Project
+ * Date        : 1/31/2023
+ * Time        : 1:53 PM
+ * Year        : 2023
+ */
+
+package lk.ijse.spring;
+
+public class WebAppInitializer {
+}
