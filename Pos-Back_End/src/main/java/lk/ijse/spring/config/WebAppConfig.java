d4edc3b763ac0ea0f988c9d84fdce91d3cb6d157
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-01-31 16:20:21 +0530

    C_Updated..

--- Pos-Back_End/src/main/java/lk/ijse/spring/config/WebAppConfig.java
+++ Pos-Back_End/src/main/java/lk/ijse/spring/config/WebAppConfig.java
@@ -16,7 +16,5 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @ComponentScan(basePackages = "lk.ijse.spring.controller")
 @EnableWebMvc
 public class WebAppConfig {
-    public WebAppConfig() {
-        System.out.println("App config");
-    }
+
 }
