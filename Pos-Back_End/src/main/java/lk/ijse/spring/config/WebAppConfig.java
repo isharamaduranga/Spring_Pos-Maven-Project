fba64461a8b55d35c7a2d7fb33348243b22e4d49
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-01-31 21:23:13 +0530

    C_Updated..

--- Pos-Back_End/src/main/java/lk/ijse/spring/config/WebAppConfig.java
+++ Pos-Back_End/src/main/java/lk/ijse/spring/config/WebAppConfig.java
@@ -13,7 +13,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@ComponentScan(basePackages = "lk.ijse.spring.controller")
+@ComponentScan(basePackageClasses = {})
 @EnableWebMvc
 public class WebAppConfig {
 
--- Pos-Back_End/src/main/java/lk/ijse/spring/config/WebRootConfig.java
+++ Pos-Back_End/src/main/java/lk/ijse/spring/config/WebRootConfig.java
@@ -7,11 +7,16 @@
  */
 
 package lk.ijse.spring.config;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
 @ComponentScan(basePackages = "lk.ijse.spring.service")
+
 public class WebRootConfig {
+
 
 }
