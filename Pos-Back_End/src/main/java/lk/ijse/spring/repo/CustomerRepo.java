1e60840997b37d397bee87d7453b59bc3c128667
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-02-01 11:20:34 +0530

    Apply two paramiters CustomerRepo for Introduced all entity and entity primary key dataType..

--- Pos-Back_End/src/main/java/lk/ijse/spring/repo/CustomerRepo.java
+++ Pos-Back_End/src/main/java/lk/ijse/spring/repo/CustomerRepo.java
@@ -1,5 +1,8 @@
 package lk.ijse.spring.repo;
 
-public interface CustomerRepo {
+import lk.ijse.spring.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepo extends JpaRepository<Customer,String> {
 
 }
