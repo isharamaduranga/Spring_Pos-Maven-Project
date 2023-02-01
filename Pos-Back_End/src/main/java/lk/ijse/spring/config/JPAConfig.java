8a03b0e7d3e12f1feeca4e1a6c3f8cbabcd2e673
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-02-01 10:12:42 +0530

    Created JPAConfig class for implement factory of Spring data accsess part..

---
+++ Pos-Back_End/src/main/java/lk/ijse/spring/config/JPAConfig.java
@@ -0,0 +1,12 @@
+/**
+ * @author : Ishara Maduarnga
+ * Project Name: Maven_Project
+ * Date        : 2/1/2023
+ * Time        : 10:10 AM
+ * Year        : 2023
+ */
+
+package lk.ijse.spring.config;
+
+public class JPAConfig {
+}
