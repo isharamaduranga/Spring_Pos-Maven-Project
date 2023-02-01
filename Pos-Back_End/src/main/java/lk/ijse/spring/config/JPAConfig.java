91d2764b1d52e3a30d54bac6928bb4eaad77b8ed
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-02-01 10:22:09 +0530

    Implement LocalContainerEntityManagerFactoryBean for introduced entity,DataSourse & JpaVendorAdapter to project..

--- Pos-Back_End/src/main/java/lk/ijse/spring/config/JPAConfig.java
+++ Pos-Back_End/src/main/java/lk/ijse/spring/config/JPAConfig.java
@@ -8,5 +8,20 @@
 
 package lk.ijse.spring.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+
+import javax.sql.DataSource;
+
 public class JPAConfig {
+
+    @Bean
+   public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource ds , JpaVendorAdapter jpa){
+       LocalContainerEntityManagerFactoryBean bean = new LocalContainerEntityManagerFactoryBean();
+       bean.setPackagesToScan("lk.ijse.spring.entity");
+       bean.setDataSource(ds);
+       bean.setJpaVendorAdapter(jpa);
+       return bean;
+   }
 }
