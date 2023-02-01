000708876be35717e35e4383f1cfd442faa17a21
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-02-01 11:02:06 +0530

    updated missing anotation location..

--- Pos-Back_End/src/main/java/lk/ijse/spring/config/WebAppConfig.java
+++ Pos-Back_End/src/main/java/lk/ijse/spring/config/WebAppConfig.java
@@ -8,12 +8,13 @@
 
 package lk.ijse.spring.config;
 
+import lk.ijse.spring.controller.CustomerController;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@ComponentScan(basePackageClasses = {})
+@ComponentScan(basePackageClasses = {CustomerController.class})
 @EnableWebMvc
 public class WebAppConfig {
 
