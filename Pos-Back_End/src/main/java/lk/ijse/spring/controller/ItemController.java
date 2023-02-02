6eea8a3df677efa192ef2ade5048adbae8fa3d8e
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-02-02 10:39:09 +0530

    Created ItemController class and for mapping request of item..

---
+++ Pos-Back_End/src/main/java/lk/ijse/spring/controller/ItemController.java
@@ -0,0 +1,20 @@
+/**
+ * @author : Ishara Maduarnga
+ * Project Name: Maven_Project
+ * Date        : 2/2/2023
+ * Time        : 10:30 AM
+ * Year        : 2023
+ */
+
+package lk.ijse.spring.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/item")
+@CrossOrigin
+public class ItemController {
+
+}
