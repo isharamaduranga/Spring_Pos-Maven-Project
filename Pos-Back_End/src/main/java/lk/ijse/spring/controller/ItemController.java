0d0b09c93e76af315898348424a9c3bc6962dcb3
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-02-02 11:20:10 +0530

    Fully Refactoured Item form Back End Proceed...

--- Pos-Back_End/src/main/java/lk/ijse/spring/controller/ItemController.java
+++ Pos-Back_End/src/main/java/lk/ijse/spring/controller/ItemController.java
@@ -9,32 +9,43 @@
 package lk.ijse.spring.controller;
 
 import lk.ijse.spring.dto.ItemDTO;
+import lk.ijse.spring.service.ItemService;
 import lk.ijse.spring.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/item")
 @CrossOrigin
 public class ItemController {
 
+    @Autowired
+    ItemService service;
+
     @GetMapping
     public ResponseUtil getAllItems(){
-
+        ArrayList<ItemDTO> allItems = service.getAllItems();
+        return new ResponseUtil("200","Success.",allItems);
     }
 
     @PostMapping
     public ResponseUtil saveItem(@ModelAttribute ItemDTO dto){
-
+        service.addItem(dto);
+        return new ResponseUtil("200", dto.getCode()+" Successfully Saved..",null);
     }
 
     @PutMapping
     public ResponseUtil updateItem(@RequestBody ItemDTO dto){
-
+        service.updateItem(dto);
+        return new ResponseUtil("200", dto.getCode()+" Successfully Updated..",null);
     }
 
     @DeleteMapping(params = "code")
     public ResponseUtil deleteItem(String code){
-
+        service.deleteItem(code);
+        return new ResponseUtil("200", code+" Successfully Deleted ..!",null);
     }
 
 }
--- Pos-Back_End/src/main/java/lk/ijse/spring/service/ItemServiceImpl.java
+++ Pos-Back_End/src/main/java/lk/ijse/spring/service/ItemServiceImpl.java
@@ -15,7 +15,6 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
