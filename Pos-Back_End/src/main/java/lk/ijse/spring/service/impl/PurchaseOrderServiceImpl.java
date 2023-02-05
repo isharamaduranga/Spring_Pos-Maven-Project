d60ebd7a05a746053091de76fcd9bce34dc9ba6c
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-02-05 18:51:50 +0530

    Created proceed of find and update item qty on hand after submit Order...

--- Pos-Back_End/src/main/java/lk/ijse/spring/service/impl/PurchaseOrderServiceImpl.java
+++ Pos-Back_End/src/main/java/lk/ijse/spring/service/impl/PurchaseOrderServiceImpl.java
@@ -9,14 +9,20 @@
 package lk.ijse.spring.service.impl;
 
 import lk.ijse.spring.dto.OrdersDTO;
+import lk.ijse.spring.entity.Item;
+import lk.ijse.spring.entity.OrderDetails;
 import lk.ijse.spring.entity.Orders;
+import lk.ijse.spring.repo.ItemRepo;
 import lk.ijse.spring.repo.OrderDetailsRepo;
 import lk.ijse.spring.repo.OrdersRepo;
+import lk.ijse.spring.service.ItemService;
 import lk.ijse.spring.service.PurchaseOrderService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -28,6 +34,8 @@ public class PurchaseOrderServiceImpl implements PurchaseOrderService {
     private OrderDetailsRepo orderDetailsRepo;
     @Autowired
     private ModelMapper mapper;
+    @Autowired
+    private ItemRepo itemRepo;
 
     @Override
     public void purchaseOrder(OrdersDTO dto) {
@@ -36,11 +44,26 @@ public class PurchaseOrderServiceImpl implements PurchaseOrderService {
 
         //check order already available..
         if (ordersRepo.existsById(orders.getOid())){
-            throw new RuntimeException("Order : "+orders.getOid()+ "Already Available .!!!");
+            throw new RuntimeException("Order : "+orders.getOid()+ " Already Available .!!!");
         }
         /** save order and order details (because define order entity cascade type ALL) */
 
         ordersRepo.save(orders); // If an error occur all transactions will be rolled backend
+
+
+        for (OrderDetails od : orders.getOrderDetails()) {
+            /**  find and update item qty on hand ...  */
+            Optional<Item> resp = itemRepo.findById(od.getItemCode());
+
+            if (!resp.isPresent()) {
+                throw new RuntimeException(od.getItemCode()+ ": Item Not Available !!!");
+            }
+
+            Item item = resp.get();
+            item.setQtyOnHand((item.getQtyOnHand()- od.getQty()));
+            itemRepo.save(item);
+
+        }
 
     }
 }
