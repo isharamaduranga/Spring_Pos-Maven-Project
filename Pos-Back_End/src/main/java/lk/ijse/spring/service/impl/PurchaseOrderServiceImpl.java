6f1a62e94e7d614b8835f18c7829e860b8f6c5dc
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-02-05 17:51:03 +0530

    Applied autowired repos for accsess related class into purchaseOrderImpl...

--- Pos-Back_End/src/main/java/lk/ijse/spring/service/impl/PurchaseOrderServiceImpl.java
+++ Pos-Back_End/src/main/java/lk/ijse/spring/service/impl/PurchaseOrderServiceImpl.java
@@ -9,9 +9,20 @@
 package lk.ijse.spring.service.impl;
 
 import lk.ijse.spring.dto.OrdersDTO;
+import lk.ijse.spring.repo.OrderDetailsRepo;
+import lk.ijse.spring.repo.OrdersRepo;
 import lk.ijse.spring.service.PurchaseOrderService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class PurchaseOrderServiceImpl implements PurchaseOrderService {
+
+    @Autowired
+    private OrdersRepo ordersRepo;
+    @Autowired
+    private OrderDetailsRepo orderDetailsRepo;
+    @Autowired
+    private ModelMapper mapper;
 
     @Override
     public void purchaseOrder(OrdersDTO dto) {
