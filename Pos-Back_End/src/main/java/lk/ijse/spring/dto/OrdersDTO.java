037aa5cad5144a8c991bacbcb6a911f164e03e79
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-02-05 14:03:49 +0530

    Created OrdersDTO in dto pkg with Pos_Back-End...

---
+++ Pos-Back_End/src/main/java/lk/ijse/spring/dto/OrdersDTO.java
@@ -0,0 +1,26 @@
+/**
+ * @author : Ishara Maduarnga
+ * Project Name: Maven_Project
+ * Date        : 2/5/2023
+ * Time        : 1:59 PM
+ * Year        : 2023
+ */
+
+package lk.ijse.spring.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class OrdersDTO {
+    private String oid;
+    private String cusID;
+    private LocalDate date;
+}
