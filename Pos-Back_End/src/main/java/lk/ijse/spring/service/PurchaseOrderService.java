d6d828bcd074e4602f025a381a18f8f66150418d
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-02-05 12:01:27 +0530

    Created PurchaseOrderController ,ServiceImpl and related interface with pos-back_end..

---
+++ Pos-Back_End/src/main/java/lk/ijse/spring/service/PurchaseOrderService.java
@@ -0,0 +1,4 @@
+package lk.ijse.spring.service;
+
+public interface PurchaseOrderService {
+}
---
+++ Pos-Back_End/src/main/java/lk/ijse/spring/service/impl/PurchaseOrderServiceImpl.java
@@ -0,0 +1,14 @@
+/**
+ * @author : Ishara Maduarnga
+ * Project Name: Maven_Project
+ * Date        : 2/5/2023
+ * Time        : 11:58 AM
+ * Year        : 2023
+ */
+
+package lk.ijse.spring.service.impl;
+
+import lk.ijse.spring.service.PurchaseOrderService;
+
+public class PurchaseOrderServiceImpl implements PurchaseOrderService {
+}
